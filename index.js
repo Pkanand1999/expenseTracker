9636e0dbcac9b835e1d82b46cc33962b85caa253
Pkanand1999 <singhisking810267@gmail.com>
2023-04-15 22:55:37 +0530

    db added

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const router=jsonServer.router("db.json");
 const middlewares=jsonServer.defaults();
 const port =process.env.PORT || 3001;
 
-server.use(middlewares)
+server.use(middlewares);
 server.use(router);
 server.use(port);
